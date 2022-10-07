dc64154071f468a95a15072d4b4845c42c136287
juracucumber1 <mr.gordien@mail.ru>
2022-10-07 06:37:08 +0300

    update
    add bcrypt
    to-fix-new-bugs......

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose'
 import { validationResult } from 'express-validator'
 
 
+import UserModel from './models/User.js'
+
 import { registerValidation } from './validation/auth.js'
 
-mongoose.connect('mongodb+srv://admin:admin@cluster0.pts9fgk.mongodb.net/?retryWrites=true&w=majority',
+mongoose.connect('mongodb+srv://admin:admin@cluster0.pts9fgk.mongodb.net/blog?retryWrites=true&w=majority',
     ).then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err))
 
@@ -14,15 +17,26 @@ const app = express();
 
 app.use(express.json())
 
-app.post('/auth/register', registerValidation, (req, res) => {
+app.post('/auth/register', registerValidation, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array())
     }
 
-    res.json({
-        success: true,
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+       email: req.body.email,
+       fullName: req.body.email,
+       avatarUrl: req.body.email,
+       passwordHash: req.body.email,
     });
+
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 app.listen(4444, (err) => {
